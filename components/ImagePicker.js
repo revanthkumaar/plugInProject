27224201f94b35c5d8f3fd154e25f27cf739b631
revanthkumaar <iamrevanthkumar@gmail.com>
2021-05-25 12:10:10 +0530

    asdas
    dasd

--- components/ImagePicker.js
+++ components/ImagePicker.js
@@ -1,36 +1,64 @@
-import { Alert, ImagePickerIOS } from "react-native"
-
-
+import React from 'react';
+import { View, Button, Image, Text, StyleSheet, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions'
+import * as Permissions from 'expo-permissions';
+
+import Colors from '../constants/Colors';
 
 const ImgPicker = props => {
-
-    //permission
-
-    const verifyPermissions = async() => {
-        const result = await Permissions.askAsync(Permissions.CAMERA)
-        if(result.status !== 'granted'){
-            Alert.alert(
-                'Permissions arent granted!',
-                'give permissions to use the app',
-                [{text:'Okay'}]);
-            return false;
-        }
-        return true;
+  const verifyPermissions = async () => {
+    const result = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (result.status !== 'granted') {
+      Alert.alert(
+        'Insufficient permissions!',
+        'You need to grant camera permissions to use this app.',
+        [{ text: 'Okay' }]
+      );
+      return false;
     }
+    return true;
+  };
 
-    //picker function 
-    const takeImageHandler = async() => {
-       const hasPermission =  await verifyPermissions();
-       if(!hasPermission){
-           return;
-       }
-        ImagePicker.launchCameraAsync()
+  const takeImageHandler = async () => {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) {
+        return;
     }
+    ImagePicker.launchCameraAsync();
+  };
 
-    //UI 
+  return (
+    <View style={styles.imagePicker}>
+      <View style={styles.imagePreview}>
+        <Text>No image picked yet.</Text>
+        <Image style={styles.image} />
+      </View>
+      <Button
+        title="Take Image"
+        color={Colors.primary}
+        onPress={takeImageHandler}
+      />
+    </View>
+  );
+};
 
-    <Button onPress ={ }> </Button>
+const styles = StyleSheet.create({
+  imagePicker: {
+    alignItems: 'center'
+  },
+  imagePreview: {
+    width: '100%',
+    height: 200,
+    marginBottom: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#ccc',
+    borderWidth: 1
+  },
+  image: {
+    width: '100%',
+    height: '100%'
+  }
+});
 
-}
+export default ImgPicker;
