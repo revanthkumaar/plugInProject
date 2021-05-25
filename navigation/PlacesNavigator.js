6f88a084dea39f6d03e69a24fb65be86fb5860f6
revanthkumaar <iamrevanthkumar@gmail.com>
2021-05-25 11:11:17 +0530

    asdlkl
    kjqljkaldk

--- navigation/PlacesNavigator.js
+++ navigation/PlacesNavigator.js
@@ -1,14 +1,12 @@
-//dependencies
 import { Platform } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
-//screens
+
 import PlacesListScreen from '../screens/PlacesListScreen';
 import PlaceDetailScreen from '../screens/PlaceDetailScreen';
 import NewPlaceScreen from '../screens/NewPlaceScreen';
 import MapScreen from '../screens/MapScreen';
 import Colors from '../constants/Colors';
 
-//configurations
 const PlacesNavigator = createStackNavigator(
   {
     Places: PlacesListScreen,
