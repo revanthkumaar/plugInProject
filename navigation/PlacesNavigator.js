9583bd9318d96abbe8f21570cb67cef60071e36c
revanthkumaar <iamrevanthkumar@gmail.com>
2021-05-25 09:59:16 +0530

    oajdljel
    kjlajkdlas

--- navigation/PlacesNavigator.js
+++ navigation/PlacesNavigator.js
@@ -1,36 +1,28 @@
-//dependencies 
-import {createStackNavigator, createAppContainer} from 'react-navigation';
-
-
-//screens you want to navigate to
+//dependencies
+import { Platform } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+//screens
 import PlacesListScreen from '../screens/PlacesListScreen';
 import PlaceDetailScreen from '../screens/PlaceDetailScreen';
 import NewPlaceScreen from '../screens/NewPlaceScreen';
 import MapScreen from '../screens/MapScreen';
 
-
+//configurations
 const PlacesNavigator = createStackNavigator(
-
-    {
-        Places: PlacesListScreen,
-        PlaceDetailScreen: PlaceDetailScreen,
-        NewPlaceScreen: NewPlaceScreen,
-        Map:MapScreen
-    },
-    {
-
-    
+  {
+    Places: PlacesListScreen,
+    PlaceDetail: PlaceDetailScreen,
+    NewPlace: NewPlaceScreen,
+    Map: MapScreen
+  },
+  {
     defaultNavigationOptions: {
-        headerStyle: {
-           backgroundColor:  Platform.OS === 'android' ? Colors.primary: ''}
-    },
-    headerTintColor: Platform.OS === 'android' ? 'white': ''
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+      },
+      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+    }
+  }
+);
 
-}
-}
-    
-    )
-
-
-
-export default createAppContainer(PlacesNavigator)
+export default createAppContainer(PlacesNavigator);
