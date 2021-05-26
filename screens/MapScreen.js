07510daa18055174a550376ee5683a19e9aa4507
revanthkumaar <iamrevanthkumar@gmail.com>
2021-05-26 11:34:47 +0530

    asdas
    dasd

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,12 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
 const MapScreen = props => {
-
-
   const [selectedLocation, setSelectedLocation] = useState();
-
 
   const mapRegion = {
     latitude: 37.78,
@@ -19,45 +16,29 @@ const MapScreen = props => {
     setSelectedLocation({
       lat: event.nativeEvent.coordinate.latitude,
       lng: event.nativeEvent.coordinate.longitude
-      
     });
-  }
-;
-
-
-
-let markerCoordinates;
-
-if (selectedLocation) {
-  markerCoordinates = {
-    latitude: selectedLocation.lat,
-    longitude: selectedLocation.lng
   };
-}
 
+  let markerCoordinates;
 
+  if (selectedLocation) {
+    markerCoordinates = {
+      latitude: selectedLocation.lat,
+      longitude: selectedLocation.lng
+    };
+  }
 
   return (
-
-    <MapView 
-  style={styles.map} 
-  region={mapRegion} 
-  onPress={selectLocationHandler}
-
-  >
-
-
-  <Marker title="Picked Location" coordinate={markerCoordinates}></Marker>
-
-  </MapView>
-
-
-  )
-  
-  
-  
-  
-  ;
+    <MapView
+      style={styles.map}
+      region={mapRegion}
+      onPress={selectLocationHandler}
+    >
+      {markerCoordinates && (
+        <Marker title="Picked Location" coordinate={markerCoordinates} />
+      )}
+    </MapView>
+  );
 };
 
 const styles = StyleSheet.create({
